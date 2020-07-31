c2c8dfe7226b8cabad24e83d030c5be01e34b49a
Chris Faux <chrisfaux95@gmail.com>
2020-07-31 13:17:21 -0400

    finished renderHours and removed <table>

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -20,27 +20,50 @@ function renderHours() {
     endMoment has a value of 1 minute
     to include the last time slot */
 
-    let currentMoment = moment({hour: startTime, minutes: 0, seconds: 0});
-    let endMoment = moment({hour: endTime, minutes: 1, seconds: 0});
+    let indexMoment = moment({ hour: startTime, minutes: 0, seconds: 0 });
+    let endMoment = moment({ hour: endTime, minutes: 1, seconds: 0 });
     let duration = moment.duration(durationMinutes, "minute")
 
     // Creates the table and appends it to main
-    let timeTable = $("<table>");
-    timeTable.appendTo($("main"));
+    let timeTable = $("main");
 
     /* Loops until the current moment
     is greater than the endMoment,
     creating the table rows and filling them */
-    while(currentMoment < endMoment){
+    while (indexMoment < endMoment) {
         // Creates the current row and appends to table
-        let timeDiv = $("<tr>");
+        let timeDiv = $("<div>");
+        timeDiv.addClass("row my-1");
         timeDiv.appendTo(timeTable);
 
+
+        currentTime = indexMoment.format("h:mmA");
         // Creates and appends the time label
-        timeDiv.append($("<td>").text(currentMoment.format("h:mmA")));
-        
-        // Increment currentMoment
-        currentMoment.add(duration);
+        let timeLabel = $("<div>").text(currentTime);
+        timeLabel.addClass("col-2");
+        timeLabel.attr("data-time", currentTime);
+        // timeLabel.append($("<hr>"))
+        timeDiv.append(timeLabel);
+
+        // Creates Text Input Area
+        let planInput = $("<textarea>");
+        planInput.addClass("col-8");
+        planInput.attr("data-time", currentTime);
+        timeDiv.append(planInput);
+
+        //Creates Saving Button
+        let planSaveBtn = $("<button>");
+        planSaveBtn.addClass("btn btn-info col-1 mx-auto");
+        planSaveBtn.attr("data-time", currentTime);
+        timeDiv.append(planSaveBtn);
+
+        // Add save icon
+        let saveIcon = $("<i>");
+        saveIcon.addClass("fas fa-save fa-2x");
+        planSaveBtn.append(saveIcon);
+
+        // Increment indexMoment
+        indexMoment.add(duration);
     }
 };
 
