24e696d7c55e753e42ff3bdaf60b09ff9b3ed887
Chris Faux <chrisfaux95@gmail.com>
2020-07-31 11:24:56 -0400

    created function to create timetable

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,26 @@
+var startTime = 9;
+var endTime = 17;
+var durationMinutes = 30;
 
 function setCurrentDay() {
     var day = moment().format("dddd, MMMM Do");
     $("#current-day").text(day);
 }
 
+function renderHours() {
+    let startMoment = moment({hour: startTime});
+    let endMoment = moment({hour: endTime, minutes: 1});
+    let duration = moment.duration(durationMinutes, "minute")
+    // console.log(startMoment);
+    let timeTable = $("<table>");
+    timeTable.appendTo($("main"));
+    while(startMoment < endMoment){
+        let timeDiv = $("<tr>");
+        timeDiv.append($("<td>").text(startMoment.format("h:mmA")));
+        timeDiv.appendTo(timeTable);
+        startMoment.add(duration);
+    }
+};
+
 setCurrentDay();
+renderHours();
