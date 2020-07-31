0b227a88b609685111de7afa51a6c9b27d01a4bf
Chris Faux <chrisfaux95@gmail.com>
2020-07-31 11:33:58 -0400

    added comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,27 +4,42 @@ var endTime = 20;
 var durationMinutes = 30;
 
 
-//Sets the header to include the current day
+// Sets the header to include the current day
 function setCurrentDay() {
     var day = moment().format("dddd, MMMM Do");
     $("#current-day").text(day);
 }
 
-//creates the table for the planner
+
+// Creates the table for the planner
 function renderHours() {
+
+    /* Set the values for the index time,
+    end time, and the incrementing duration 
+    
+    endMoment has a value of 1 minute
+    to include the last time slot */
+
     let currentMoment = moment({hour: startTime});
     let endMoment = moment({hour: endTime, minutes: 1});
     let duration = moment.duration(durationMinutes, "minute")
-    //
+
+    // Creates the table and appends it to main
     let timeTable = $("<table>");
     timeTable.appendTo($("main"));
+
+    /* Loops until the current moment
+    is greater than the endMoment,
+    creating the table rows and filling them */
     while(currentMoment < endMoment){
-        //create the row
+        // Creates the current row and appends to table
         let timeDiv = $("<tr>");
-        //create and append the time at beginning of row
-        timeDiv.append($("<td>").text(currentMoment.format("h:mmA")));
         timeDiv.appendTo(timeTable);
-        //increase time of 
+
+        // Creates and appends the time label
+        timeDiv.append($("<td>").text(currentMoment.format("h:mmA")));
+        
+        // Increment currentMoment
         currentMoment.add(duration);
     }
 };
